fcb6043c31778bc97385fbe6b4cb31c830d7b5b7
navadeep <navadeepsatheesh@gmail.comm>
2023-11-18 23:11:28 +0530

    completed hero and about

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Image from "next/image"
+
+// import "@/styles/hero.css"
+
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero">
+
+      <div className="hero_sub">
+
+        <div className="hero_logo_section">
+
+          <img src="/logo_hero.png" alt="image" />
+          <h3>COMING SOON</h3>
+
+        </div>
+
+        <h1 className="hero_caption">Legacy is back</h1>
+
+        <div className="about">
+          <h3>ABOUT THE EVENT</h3>
+          <p>
+            <b>Dotslash</b> is the annual technical event held by department of computer science & engineering, college of engineering trivandrum
+          </p>
+       
+       
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
