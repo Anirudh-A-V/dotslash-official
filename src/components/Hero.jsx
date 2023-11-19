57c90ce3663c92e61b2d3becedbfbc832aca562c
Alfeen <Alfeen@Alfeens-Mac-mini.local>
2023-11-19 23:11:30 +0530

    video animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,36 +1,49 @@
-import React from 'react'
-import Image from "next/image"
-import localFont from 'next/font/local'
+import React from "react";
+import Image from "next/image";
+import localFont from "next/font/local";
 
 const PPNeueMachinaFont = localFont({
   src: [
     {
-      path: '../../public/fonts/PPNeueMachina-Light.otf',
-      weight: '400',
-      style: 'normal',
+      path: "../../public/fonts/PPNeueMachina-Light.otf",
+      weight: "400",
+      style: "normal",
     },
   ],
-})
-
-
+});
 
 const Hero = () => {
   return (
-    <div className='flex relative justify-center items-center flex-col w-full h-screen'>
-      <div className='w-full lg:min-w-[768px] flex justify-center items-center flex-col'>
-        <Image src='/hero-logo.svg' alt='hero' width={700} height={128} style={{
-          objectFit: 'cover',
-          objectPosition: 'center'
-        }} />
-        <h2 className={`text-white font-normal text-[12px] lg:text-[16px] leading-normal tracking-[0.6em] lg:tracking-[1em] px-1 ml-2 mt-4 ${PPNeueMachinaFont.className}`}>ANNUAL TECHNO-CULTURAL FEST</h2>
+    <div className="flex relative justify-center items-center flex-col w-full h-screen">
+      <div className="w-full lg:min-w-[768px] flex justify-center items-center flex-col">
+        <Image
+          src="/hero-logo.svg"
+          alt="hero"
+          width={700}
+          height={128}
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+        <h2
+          className={`text-white font-normal text-[12px] lg:text-[16px] leading-normal tracking-[0.6em] lg:tracking-[1em] px-1 ml-2 mt-4 ${PPNeueMachinaFont.className}`}
+        >
+          ANNUAL TECHNO-CULTURAL FEST
+        </h2>
       </div>
-      <div className='absolute inset-0 w-full h-full flex justify-center items-center'>
-        <video className='-z-10 w-full h-1/3 lg:w-1/2 lg:h-1/2 object-cover' autoPlay loop muted>
-          <source src='/videos/loop.mp4' type='video/mp4' />
+      {/* <div className="absolute inset-0 w-full h-full flex justify-center items-center">
+        <video
+          className="-z-10 w-full h-1/3 lg:w-1/2 lg:h-1/2 object-cover"
+          autoPlay
+          muted
+          onEnded={() => setMainContent(true)}
+        >
+          <source src="/videos/loop.mp4" type="video/mp4" />
         </video>
-      </div>
+      </div> */}
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,22 +8,36 @@ import Events from '@/components/Events'
 import Sponsors from '@/components/Sponsors'
 import Footer from '@/components/Footer'
 import About from '@/components/About'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [mainContent, setMainContent] = useState(false)
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-start w-full ${inter.className}`}
-    >
-      <Navbar />
-      <Hero />
-      <About />
-      {/* <Featured />
-      <PreEvents />
-      <Events />
-      <Sponsors />
-      <Footer /> */}
-    </main>
+    <div className='relative overflow-hidden'>
+      <div className='flex relative justify-center items-center flex-col w-full h-screen'>
+        <div className='absolute inset-0 w-full h-full flex justify-center items-center'>
+          <video className='w-full h-full object-cover' autoPlay muted
+            onEnded={() => setMainContent(true)}
+          >
+            <source src='/videos/full.mp4' type='video/mp4' />
+          </video>
+        </div>
+      </div>
+      <main
+        className={`flex slide-content min-h-screen flex-col items-center justify-start w-full ${mainContent ? 'slide-in' : 'slide-out'}`}
+      >
+
+        <Navbar />
+        <Hero />
+        <About />
+        {/* <Featured />
+        <PreEvents />
+        <Events />
+        <Sponsors />
+        <Footer /> */}
+      </main>
+    </div>
   )
 }
