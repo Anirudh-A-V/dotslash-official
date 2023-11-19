52c47ea34cf14f8fb18fe718584ea0fe91e271f5
Athul Anoop <athulanoop2020@gmail.com>
2023-11-19 23:39:33 +0530

    Updated the landing page transition (intro) and smoothed out the transition to the loop
    intro-loop transition is a bit janky, but the artifacting is not very noticeable

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,36 +1,61 @@
-import React from 'react'
-import Image from "next/image"
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const PPNeueMachinaFont = localFont({
-  src: [
-    {
-      path: '../../public/fonts/PPNeueMachina-Light.otf',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
-})
-
-
+    src: [
+        {
+            path: "../../public/fonts/PPNeueMachina-Light.otf",
+            weight: "400",
+            style: "normal",
+        },
+    ],
+});
 
 const Hero = () => {
-  return (
-    <div className='flex relative justify-center items-center flex-col w-full h-screen'>
-      <div className='w-full lg:min-w-[768px] flex justify-center items-center flex-col'>
-        <Image src='/hero-logo.svg' alt='hero' width={700} height={128} style={{
-          objectFit: 'cover',
-          objectPosition: 'center'
-        }} />
-        <h2 className={`text-white font-normal text-[12px] lg:text-[16px] leading-normal tracking-[0.6em] lg:tracking-[1em] px-1 ml-2 mt-4 ${PPNeueMachinaFont.className}`}>ANNUAL TECHNO-CULTURAL FEST</h2>
-      </div>
-      <div className='absolute inset-0 w-full h-full flex justify-center items-center'>
-        <video className='-z-10 w-full h-1/3 lg:w-1/2 lg:h-1/2 object-cover' autoPlay loop muted>
-          <source src='/videos/loop.mp4' type='video/mp4' />
-        </video>
-      </div>
-    </div>
-  )
-}
+    const [firstTime, setFirstTime] = useState(true);
+    const [loopVid, setLoopVid] = useState(false);
+    return (
+        <div className="flex justify-center items-center flex-col w-full h-screen overflow-hidden">
+            <video
+                className={
+                    firstTime
+                        ? "hero_intro_vid absolute z-[2] w-screen h-screen object-cover"
+                        : "absolute hidden"
+                }
+                src="videos/intro.mp4"
+                muted
+                autoPlay
+                onEnded={() => {
+                    setFirstTime((firstTime) => !firstTime);
+                    setLoopVid((loopVid) => !loopVid);
+                }}
+            />
+            <video
+                className={
+                    loopVid
+                        ? "hero_loop_vid scale-[70%] absolute h-full object-cover"
+                        : "absolute hidden"
+                }
+                src="videos/loop.mp4"
+                muted
+                autoPlay
+                loop
+            />
+            <div className="hero_intro_text absolute z-10 bottom-[10%] flex flex-col gap-4 justify-center items-center">
+                <img
+                    className="flex-shrink-0 w-full"
+                    src="/hero-logo.svg"
+                    alt="hero logo"
+                />
+                <h2
+                    className={`text-center text-white font-normal text-[12px] lg:text-[16px] leading-normal tracking-[0.6em] lg:tracking-[1em]${PPNeueMachinaFont.className}`}
+                >
+                    ANNUAL TECHNO-CULTURAL FEST
+                </h2>
+            </div>
+        </div>
+    );
+};
 
-export default Hero
+export default Hero;
