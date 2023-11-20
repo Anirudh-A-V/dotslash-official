930cdb0150d7231442ecbbccef9a85c409c25828
Athul Anoop <athulanoop2020@gmail.com>
2023-11-20 18:51:23 +0530

    Optimised landing page intro transition

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const PPNeueMachinaFont = localFont({
     src: [
@@ -13,33 +13,32 @@ const PPNeueMachinaFont = localFont({
 });
 
 const Hero = () => {
-    const [firstTime, setFirstTime] = useState(true);
-    const [loopVid, setLoopVid] = useState(false);
+    const [introPlayed, setIntroPlayed] = useState(false);
+    const loopRef = useRef(null);
+    useEffect(() => {
+        if (introPlayed) {
+            loopRef.current.muted = true;
+            loopRef.current.autoPlay = true;
+            loopRef.current.play();
+        }
+    }, [introPlayed]);
+
     return (
-        <div className="flex justify-center items-center flex-col w-full h-screen overflow-hidden">
+        <div className="flex flex-col justify-center items-center w-full h-screen overflow-hidden">
             <video
-                className={
-                    firstTime
-                        ? "hero_intro_vid absolute z-[2] w-screen h-screen object-cover"
-                        : "absolute hidden"
-                }
+                className={`${
+                    introPlayed ? "z-[0]" : "z-[2]"
+                } hero_intro_vid absolute w-screen h-screen object-cover duration-150 ease-in-out`}
                 src="videos/intro.mp4"
                 muted
                 autoPlay
-                onEnded={() => {
-                    setFirstTime((firstTime) => !firstTime);
-                    setLoopVid((loopVid) => !loopVid);
-                }}
+                onEnded={() => setIntroPlayed((state) => !state)}
             />
             <video
-                className={
-                    loopVid
-                        ? "hero_loop_vid scale-[70%] absolute h-full object-cover"
-                        : "absolute hidden"
-                }
+                ref={loopRef}
+                className="z-[1] hero_loop_vid scale-[70%] absolute w-screen h-screen object-cover"
                 src="videos/loop.mp4"
                 muted
-                autoPlay
                 loop
             />
             <div className="hero_intro_text absolute z-10 bottom-[10%] flex flex-col gap-4 justify-center items-center">
