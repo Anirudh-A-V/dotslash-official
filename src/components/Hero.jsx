64cfc093534ac3e9b3294d36de40921bd63820a4
Athul Anoop <athulanoop2020@gmail.com>
2023-11-20 22:04:30 +0530

    Improved responsiveness of the landing intro transition
    Temporary fix that works only on chrome. All other browsers may or may not display the changes correctly

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,19 @@ const PPNeueMachinaFont = localFont({
 const Hero = () => {
     const [introPlayed, setIntroPlayed] = useState(false);
     const loopRef = useRef(null);
+    const loopRefPortrait = useRef(null);
     useEffect(() => {
         if (introPlayed) {
-            loopRef.current.muted = true;
-            loopRef.current.autoPlay = true;
-            loopRef.current.play();
+            if (loopRef.current) {
+                loopRef.current.muted = true;
+                loopRef.current.autoPlay = true;
+                loopRef.current.play();
+            }
+            if (loopRefPortrait.current) {
+                loopRefPortrait.current.muted = true;
+                loopRefPortrait.current.autoPlay = true;
+                loopRefPortrait.current.play();
+            }
         }
     }, [introPlayed]);
 
@@ -27,26 +35,38 @@ const Hero = () => {
         <div className="flex flex-col justify-center items-center w-full h-screen overflow-hidden">
             <video
                 className={`${
-                    introPlayed ? "z-[0]" : "z-[2]"
-                } hero_intro_vid absolute w-screen h-screen object-cover duration-150 ease-in-out`}
+                    introPlayed ? "z-[0]" : "md:z-[5] z-[0]"
+                } md:flex hidden hero_intro_vid_md absolute w-screen h-screen object-cover duration-150 ease-in-out`}
                 src="videos/intro.mp4"
+                muted
+                autoPlay
+                onEnded={() => setIntroPlayed((state) => true)}
+            />
+            <video
+                ref={loopRef}
+                className="md:z-[4] z-0 md:flex hidden hero_loop_vid_md scale-[70%] absolute w-screen h-screen object-cover"
+                src="videos/loop.mp4"
+                muted
+                loop
+            />
+            <video
+                className={`${
+                    introPlayed ? "z-[0]" : "md:z-0 z-[3]"
+                } md:hidden flex hero_intro_vid absolute w-screen h-screen object-cover duration-150 ease-in-out  border-green-400`}
+                src="videos/intro-portrait.mp4"
                 muted
                 autoPlay
                 onEnded={() => setIntroPlayed((state) => !state)}
             />
             <video
-                ref={loopRef}
-                className="z-[1] hero_loop_vid scale-[70%] absolute w-screen h-screen object-cover"
-                src="videos/loop.mp4"
+                ref={loopRefPortrait}
+                className="md:z-0 md:hidden flex z-[2] hero_loop_vid scale-[90%] absolute w-screen h-screen object-cover  border-green-400"
+                src="videos/loop-portrait.mp4"
                 muted
                 loop
             />
             <div className="hero_intro_text absolute z-10 bottom-[10%] flex flex-col gap-4 justify-center items-center">
-                <img
-                    className="flex-shrink-0 w-full"
-                    src="/hero-logo.svg"
-                    alt="hero logo"
-                />
+                <img className=" w-full" src="/hero-logo.svg" alt="hero logo" />
                 <h2
                     className={`text-center text-white font-normal text-[12px] lg:text-[16px] leading-normal tracking-[0.6em] lg:tracking-[1em]${PPNeueMachinaFont.className}`}
                 >
