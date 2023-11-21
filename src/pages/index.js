c1332217089f15e58bfdfba5a87ce76c73a3d880
Athul Anoop <athulanoop2020@gmail.com>
2023-11-21 17:03:24 +0530

    Landing intro is now responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,29 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import Hero from '@/components/Hero'
-import Featured from '@/components/Featured'
-import PreEvents from '@/components/PreEvents'
-import Events from '@/components/Events'
-import Sponsors from '@/components/Sponsors'
-import Footer from '@/components/Footer'
-import About from '@/components/About'
+import About from "@/components/About";
+import Events from "@/components/Events";
+import Featured from "@/components/Featured";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import PreEvents from "@/components/PreEvents";
+import Sponsors from "@/components/Sponsors";
+import { Inter } from "next/font/google";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-start w-full ${inter.className}`}
-    >
-      <Navbar />
-      <Hero />
-      <About />
-      {/* <Featured />
+    return (
+        <main
+            className={`flex min-h-screen flex-col items-center justify-start w-full ${inter.className}`}
+        >
+            <Navbar />
+            <Hero />
+            <About />
+            {/* <Featured />
       <PreEvents />
       <Events />
       <Sponsors />
       <Footer /> */}
-    </main>
-  )
+        </main>
+    );
 }
