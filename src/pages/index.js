1d95651f0987d3f93b7f2add43054e78032a8a4c
MVK2803 <mathewvinukariath@gmail.com>
2023-11-21 19:43:58 +0530

    Carousel for all type of Events added

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,12 @@ import Sponsors from '@/components/Sponsors'
 import Footer from '@/components/Footer'
 import About from '@/components/About'
 
+import {workshopData,preEventData,competitionData,informalsData} from "../assets/data/data.js"
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-start w-full ${inter.className}`}
@@ -19,9 +22,15 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
-      <Events />
-      {/* <Featured />
-      <PreEvents />
+      
+      {workshopData.length ? <Events eventData={workshopData} sectionTitle="WORKSHOPS" /> : null}
+      {preEventData.length ? <Events eventData={preEventData} sectionTitle="PRE EVENTS" /> : null}
+      {competitionData.length ? <Events eventData={competitionData} sectionTitle="COMPETITIONS" /> : null}
+      {informalsData.length ? <Events eventData={informalsData} sectionTitle="INFORMALS" /> : null}
+      
+      
+      
+      {/* 
       
       <Sponsors />
       <Footer /> */}
