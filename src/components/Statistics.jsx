e42a8420fb8636a26f7c7b04dc6b1471d87b4c25
Anirudh A V <anirudh.av02@gmail.com>
2023-11-22 09:50:22 +0530

    Update About and Statistics components

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -17,12 +17,12 @@ function Statistics() {
 
     return (
 
-        <div className="statistics relative w-full overflow-clip md:py-16">
+        <div className="statistics relative w-full overflow-clip md:py-16 mt-10 md:mt-0">
 
 
             <img src={'images/stats_torous.png'} alt="torous" className="torous absolute w-[180px] h-[180px] md:w-[300px] md:h-[400px] -z-[1] -right-12 top-24" />
 
-            <div className="stats_heading flex justify-center items-center">
+            <div className="stats_heading flex justify-center items-center mr-10 mb-2">
                 <img src={'images/stats_gems.png'} alt="gem" className="gem " />
                 <h2 className={`text-[16px] md:text-2xl font-normal text-center text-white ${PPNeueMontrealFont.className}`}>
                     this year we bring
