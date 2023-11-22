60ca852feb28b8cb739503f517d0e69c421b9891
Athul Anoop <athulanoop2020@gmail.com>
2023-11-22 18:22:13 +0530

    Updated the video tags to have variable height
    Width has been set to window width, but height is variable. This prevents clipping on the edges of the video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,13 +35,13 @@ const Hero = () => {
         } else {
             setWindowWidth(() => window.innerWidth);
         }
-    });
+    }, []);
     return (
         <div className="flex flex-col justify-center items-center w-full h-screen overflow-hidden">
             <video
                 className={`${
                     introPlayed ? "opacity-0" : ""
-                } z-[1] hero_intro_vid absolute w-screen h-screen object-cover duration-150 ease-in-out`}
+                } z-[1] hero_intro_vid absolute object-cover w-full duration-150 ease-in-out`}
                 src={
                     windowWidth >= 768
                         ? "videos/intro.mp4"
@@ -49,17 +49,19 @@ const Hero = () => {
                 }
                 muted
                 autoPlay
+                playsInline
                 onEnded={() => setIntroPlayed((state) => true)}
             />
             <video
                 ref={loopRef}
-                className="md:scale-[70%] scale-[90%]  absolute w-screen h-screen object-cover"
+                className="md:scale-[70%] scale-[90%] absolute object-cover w-full"
                 src={
                     windowWidth >= 768
                         ? "videos/loop.mp4"
                         : "videos/loop-portrait.mp4"
                 }
                 muted
+                playsInline
                 loop
             />
             <div className="hero_intro_text absolute z-10 flex flex-col gap-4 justify-center items-center">
