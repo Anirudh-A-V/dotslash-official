973e4cf37f833268c04dad1d05ac78b6957231bf
Anirudh A V <anirudh.av02@gmail.com>
2023-11-22 10:31:00 +0530

    Add meta tags for SEO and social media sharing

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,25 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
-        <Html lang="en" style={{scrollBehavior:'smooth'}}>
-            <Head />
+        <Html lang="en" style={{ scrollBehavior: 'smooth' }}>
+            <Head>
+                <title>DotSlash '23</title>
+                <meta name="description" content="This is the official website of DotSlash CET 2023." />
+                <meta name="keywords" content="dotslash, DotSlash, Dot Slash, CET, CSE, CET CSE, College of Engineering Trivandrum, Computer Science & Engineering, Thiruvananthapuram, Trivandrum, Techfest, Technical fest, Cultural fest" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="robots" content="index, follow" />
+
+                <meta property="og:title" content="DotSlash '23" />
+                <meta property="og:description" content="Official website of the technical fest of CSE CET" />
+                <meta property="og:image" content="../../public/images/metalogo.png" />
+                <meta property="og:url" content="https://www.dotslashcet.tech/" />
+
+                <meta name="twitter:card" content="../../public/images/metalogo.png" />
+                <meta name="twitter:title" content="DotSlash '23" />
+                <meta name="twitter:description" content="Official website of the technical fest of CSE CET" />
+                <meta name="twitter:image" content="../../public/images/metalogo.png" />
+
+            </Head>
             <body>
                 <Main />
                 <NextScript />
--- src/pages/events/pre-events/index.js
+++ src/pages/events/pre-events/index.js
@@ -1,8 +1,10 @@
-import Navbar from '@/components/Navbar'
 import React, { useState, Fragment } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import localFont from "next/font/local";
+import Head from 'next/head';
 import Link from 'next/link';
+import localFont from "next/font/local";
+
+import Navbar from '@/components/Navbar'
+import { Dialog, Transition } from '@headlessui/react'
 
 import { preEventData } from '../../../assets/data/data';
 
@@ -46,126 +48,146 @@ const PreEventsPage = () => {
     }
 
     return (
-        <main
-            className={`flex min-h-screen flex-col items-center justify-start w-full bg-black`}
-        >
-            <Navbar />
-            <h1 className={`text-white text-4xl font-medium mt-20 mb-8 ${PPNeueMontrealFont.className}`}>PRE-EVENTS</h1>
-            <div className='flex flex-col md:flex-row flex-wrap gap-8 md:gap-4 justify-center items-center'>
-                {preEventData?.map((event, index) => (
-                    <div className="w-[300px] p-0 md:pr-2">
-                        <div className="w-full h-[360px] bg-lime-200">
-                            <img
-                                className="w-full h-full"
-                                src={event.eventPoster.src}
-                                alt="Event Poster"
-                            />
-                        </div>
-                        <div className="mb-2 w-full p-[24px] pt-[12px] pb-[12px] justify-between flex  bg-[#090C53]">
-                            <div className="my-auto">
-                                <button className="text-[14px] lg:text-[16px] font-[500] text-[#D4DDFF]"
-                                    onClick={(e) => openModal(event)}
-                                >
-                                    View Details{" "}
-                                </button>
+        <>
+            <Head>
+                <title>Pre-events</title>
+                <meta name="description" content="List of pre-events of DotSlash '23." />
+                <meta name="keywords" content="dotslash, DotSlash, Dot Slash, CET, CSE, CET CSE, College of Engineering Trivandrum, Computer Science & Engineering, Thiruvananthapuram, Trivandrum, Techfest, Technical fest, Cultural fest" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="robots" content="index, follow" />
+
+                <meta property="og:title" content="DotSlash '23" />
+                <meta property="og:description" content="Official website of the technical fest of CSE CET" />
+                <meta property="og:image" content="../../../../public/images/metalogo.png" />
+                <meta property="og:url" content="https://www.dotslashcet.tech/" />
+
+                <meta name="twitter:card" content="../../../../public/images/metalogo.png" />
+                <meta name="twitter:title" content="DotSlash '23" />
+                <meta name="twitter:description" content="Official website of the technical fest of CSE CET" />
+                <meta name="twitter:image" content="../../../../public/images/metalogo.png" />
+
+            </Head>
+            <main
+                className={`flex min-h-screen flex-col items-center justify-start w-full bg-black`}
+            >
+                <Navbar />
+                <h1 className={`text-white text-4xl font-medium mt-20 mb-8 ${PPNeueMontrealFont.className}`}>PRE-EVENTS</h1>
+                <div className='flex flex-col md:flex-row flex-wrap gap-8 md:gap-4 justify-center items-center'>
+                    {preEventData?.map((event, index) => (
+                        <div className="w-[350px] h-full p-0 md:pr-2">
+                            <div className="w-full h-full bg-lime-200">
+                                <img
+                                    className="w-full h-full object-cover"
+                                    src={event.eventPoster.src}
+                                    alt="Event Poster"
+                                />
                             </div>
-                            <div>
-                                {event.eventStatus === 1 ? (
-                                    <Link
-                                        className={`text-[14px] lg:text-[16px] font-medium text-[#091A61] bg-[#C7D2FF] px-[12px] py-[6px] ${PPNeueMontrealFont.className}`}
-                                        href={event.eventRegistration}
+                            <div className="mb-2 w-full p-[24px] pt-[12px] pb-[12px] justify-between flex  bg-[#090C53]">
+                                <div className="my-auto">
+                                    <button className="text-[14px] lg:text-[16px] font-[500] text-[#D4DDFF]"
+                                        onClick={(e) => openModal(event)}
                                     >
-                                        Register
-                                    </Link>
-                                ) : (
-                                    <span className="text-[14px] lg:text-[16px] font-medium text-[#091A61] bg-[#C7D2FF] px-[12px] py-[6px] cursor-not-allowed opacity-50">
-                                        Closed
-                                    </span>
-                                )}
+                                        View Details{" "}
+                                    </button>
+                                </div>
+                                <div>
+                                    {event.eventStatus === 1 ? (
+                                        <Link
+                                            className={`text-[14px] lg:text-[16px] font-medium text-[#091A61] bg-[#C7D2FF] px-[12px] py-[6px] ${PPNeueMontrealFont.className}`}
+                                            href={event.eventRegistration}
+                                        >
+                                            Register
+                                        </Link>
+                                    ) : (
+                                        <span className="text-[14px] lg:text-[16px] font-medium text-[#091A61] bg-[#C7D2FF] px-[12px] py-[6px] cursor-not-allowed opacity-50">
+                                            Closed
+                                        </span>
+                                    )}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
-            {eventModal && (
-                <Transition appear show={isOpen} as={Fragment}>
-                    <Dialog as="div" className="relative z-30" onClose={closeModal}>
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0"
-                            enterTo="opacity-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                        >
-                            <div className="fixed inset-0 bg-black/25" />
-                        </Transition.Child>
+                    ))}
+                </div>
+                {eventModal && (
+                    <Transition appear show={isOpen} as={Fragment}>
+                        <Dialog as="div" className="relative z-30" onClose={closeModal}>
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <div className="fixed inset-0 bg-black/25" />
+                            </Transition.Child>
 
-                        <div className="fixed inset-0 overflow-y-auto">
-                            <div className="flex min-h-full items-center justify-center p-4 text-center">
-                                <Transition.Child
-                                    as={Fragment}
-                                    enter="ease-out duration-300"
-                                    enterFrom="opacity-0 scale-95"
-                                    enterTo="opacity-100 scale-100"
-                                    leave="ease-in duration-200"
-                                    leaveFrom="opacity-100 scale-100"
-                                    leaveTo="opacity-0 scale-95"
-                                >
-                                    <Dialog.Panel className="w-full max-w-4xl flex flex-col transform overflow-hidden bg-[#121212] p-4 md:p-8 text-left align-middle transition-all">
-                                        <div className="flex justify-end items-center mb-4">
-                                            <button
-                                                className={`text-[14px] lg:text-[16px] font-extrabold text-white ${PPTelegraffont.className}`}
-                                                onClick={closeModal}
-                                            >
-                                                CLOSE{" "}
-                                            </button>
-                                        </div>
-                                        <div className="flex flex-col lg:flex-row justify-center items-center md:items-start gap-6">
-                                            <div className="w-full flex-1 flex justify-center items-center">
-                                                <img
-                                                    // className="w-4/5 h-4/5 md:w-full md:h-full"
-                                                    className="w-full h-full"
-                                                    src={eventModal.eventPoster.src}
-                                                    alt="Event Poster"
-                                                />
+                            <div className="fixed inset-0 overflow-y-auto">
+                                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                                    <Transition.Child
+                                        as={Fragment}
+                                        enter="ease-out duration-300"
+                                        enterFrom="opacity-0 scale-95"
+                                        enterTo="opacity-100 scale-100"
+                                        leave="ease-in duration-200"
+                                        leaveFrom="opacity-100 scale-100"
+                                        leaveTo="opacity-0 scale-95"
+                                    >
+                                        <Dialog.Panel className="w-full max-w-4xl flex flex-col transform overflow-hidden bg-[#121212] p-4 md:p-8 text-left align-middle transition-all">
+                                            <div className="flex justify-end items-center mb-4">
+                                                <button
+                                                    className={`text-[14px] lg:text-[16px] font-extrabold text-white ${PPTelegraffont.className}`}
+                                                    onClick={closeModal}
+                                                >
+                                                    CLOSE{" "}
+                                                </button>
                                             </div>
-                                            <div className="overflow-hidden flex flex-1 flex-col h-full justify-between items-start">
-                                                <div className="flex flex-col">
-                                                    <p className={`text-[16px] lg:text-[18px] ${PPNeueMontrealFont.className} font-medium w-full text-white mb-4`}>
-                                                        {eventModal.eventName}
-                                                    </p>
-                                                    <p className={`scroll text-sm md:text-lg overflow-auto h-[250px] md:h-[400px] font-normal text-white mb-4 whitespace-pre-line ${PPNeueMontrealFont.className}`}>
-                                                        {eventModal.eventDesc ? eventModal.eventDesc : "No description available"}
-                                                    </p>
+                                            <div className="flex flex-col lg:flex-row justify-center items-center md:items-start gap-6">
+                                                <div className="w-full flex-1 flex justify-center items-center">
+                                                    <img
+                                                        // className="w-4/5 h-4/5 md:w-full md:h-full"
+                                                        className="w-full h-full"
+                                                        src={eventModal.eventPoster.src}
+                                                        alt="Event Poster"
+                                                    />
                                                 </div>
+                                                <div className="overflow-hidden flex flex-1 flex-col h-full justify-between items-start">
+                                                    <div className="flex flex-col">
+                                                        <p className={`text-[16px] lg:text-[18px] ${PPNeueMontrealFont.className} font-medium w-full text-white mb-4`}>
+                                                            {eventModal.eventName}
+                                                        </p>
+                                                        <p className={`scroll text-sm md:text-lg overflow-auto h-[250px] md:h-[360px] font-normal text-white mb-4 whitespace-pre-line ${PPNeueMontrealFont.className}`}>
+                                                            {eventModal.eventDesc ? eventModal.eventDesc : "No description available"}
+                                                        </p>
+                                                    </div>
 
 
-                                                <div className="flex w-full justify-end items-center mt-2">
-                                                    {eventModal.eventStatus === 1 ? (
-                                                        <Link
-                                                            className={`text-[14px] lg:text-[16px] font-medium text-[#091A61] bg-[#C7D2FF] px-[12px] py-[6px] ${PPNeueMontrealFont.className}`}
-                                                            href={eventModal.eventRegistration}
-                                                        >
-                                                            Register
-                                                        </Link>
-                                                    ) : (
-                                                        <span className="text-[14px] lg:text-[16px] font-medium text-[#091A61] bg-[#C7D2FF] px-[12px] py-[6px] cursor-not-allowed opacity-80">
-                                                            Closed
-                                                        </span>
-                                                    )}
+                                                    <div className="flex w-full justify-end items-center mt-2">
+                                                        {eventModal.eventStatus === 1 ? (
+                                                            <Link
+                                                                className={`text-[14px] lg:text-[16px] font-medium text-[#091A61] bg-[#C7D2FF] px-[12px] py-[6px] ${PPNeueMontrealFont.className}`}
+                                                                href={eventModal.eventRegistration}
+                                                            >
+                                                                Register
+                                                            </Link>
+                                                        ) : (
+                                                            <span className="text-[14px] lg:text-[16px] font-medium text-[#091A61] bg-[#C7D2FF] px-[12px] py-[6px] cursor-not-allowed opacity-80">
+                                                                Closed
+                                                            </span>
+                                                        )}
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </Dialog.Panel>
-                                </Transition.Child>
+                                        </Dialog.Panel>
+                                    </Transition.Child>
+                                </div>
                             </div>
-                        </div>
-                    </Dialog>
-                </Transition>
-            )}
-        </main>
+                        </Dialog>
+                    </Transition>
+                )}
+            </main>
+        </>
     )
 }
 
