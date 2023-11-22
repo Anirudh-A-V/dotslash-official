82a1cfafc48544bb5c585a3a7a91e3e1e1725c72
Anirudh A V <anirudh.av02@gmail.com>
2023-11-22 10:47:26 +0530

    Add Google Analytics tracking code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from 'next/script';
 
 export default function Document() {
     return (
@@ -22,9 +23,41 @@ export default function Document() {
 
             </Head>
             <body>
+                <Script src="https://www.googletagmanager.com/gtag/js?id=G-96CQYSH4G1" />
+                <Script id="google-analytics">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+ 
+                        gtag('config', 'G-96CQYSH4G1');
+                    `}
+                </Script>
                 <Main />
                 <NextScript />
             </body>
         </Html>
     );
 }
+/*
+import Script from 'next/script'
+ 
+function Home() {
+  return (
+    <div className="container">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
+    </div>
+  )
+}
+ 
+export default Home
+*/
