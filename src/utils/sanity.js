6e0d96170f67c8e5a53a71786c93b53a4edd87bf
Anirudh A V <anirudh.av02@gmail.com>
2023-11-25 21:43:46 +0530

    Add functions to fetch different types of events and generate image URLs

--- src/utils/sanity.js
+++ src/utils/sanity.js
@@ -7,3 +7,37 @@ export const client = createClient({
     useCdn: true,
     apiVersion: '2023-10-28', // use current date (YYYY-MM-DD) to target the latest API version
 })
+
+const builder = imageUrlBuilder(client)
+
+export const getPreEvents = async () => {
+    const preEvents = await client.fetch(`*[_type == "preEvents"] | order(date desc)`)
+    return preEvents
+}
+
+export const getCompetitions = async () => {
+    const competitions = await client.fetch(`*[_type == "competitions"] | order(date desc)`)
+    return competitions
+}
+
+export const getWorkshops = async () => {
+    const workshops = await client.fetch(`*[_type == "workshops"] | order(date desc)`)
+    return workshops
+}
+
+export const getInformals = async () => {
+    const informals = await client.fetch(`*[_type == "informals"] | order(date desc)`)
+    return informals
+}
+
+export const getJuniorEvents = async () => {
+    const juniorEvents = await client.fetch(`*[_type == "juniorEvents"] | order(date desc)`)
+    return juniorEvents
+}
+
+export const getFeaturedEvents = async () => {
+    const featuredEvents = await client.fetch(`*[_type == "featuredEvents"] | order(date desc)`)
+    return featuredEvents
+}
+
+export const urlFor = (source) => builder.image(source)
