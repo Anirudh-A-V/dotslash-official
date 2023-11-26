6cc1d4e4b914a753d5b8d462efa4a00157f9f03d
Anirudh A V <anirudh.av02@gmail.com>
2023-11-26 22:06:20 +0530

    Update About component styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -33,16 +33,16 @@ const PPNeueMontrealFont = localFont({
 
 const About = () => {
   return (
-    <div className="w-full flex flex-col lg:flex-row lg:h-[300px] justify-center items-center p-6 mb-8 md:mb-0 lg:px-48 lg:py-6 lg:mb-20" id="about-us">
+    <div className="w-full flex flex-col lg:flex-row lg:h-[300px] justify-center items-center p-6 mt-8 mb-16 md:mb-0 lg:px-48 lg:py-6 lg:mt-24 lg:mb-28" id="about-us">
       <div className="flex justify-center items-center lg:justify-start flex-nowrap lg:items-start w-full lg:h-full lg:w-fit">
-        <h2 className={`text-[42px] md:text-[60px] lg:text-[78px] font-normal flex-nowrap text-center lg:text-left lg:mr-4 text-white ${PPEditotialNewFont.className} mb-2 lg:mb-0`}
+        <h2 className={`text-[42px] md:text-[60px] lg:text-[78px] font-normal flex-nowrap text-center lg:text-left lg:mr-2 text-white ${PPEditotialNewFont.className} mb-2 lg:mb-0`}
           style={{ whiteSpace: "nowrap" }}
         >
           Legacy is back
         </h2>
       </div>
       <div className="flex flex-col justify-center items-center lg:items-baseline w-full lg:h-full ">
-        <p className={`text-[14px] md:text-[16px] md:w-full lg:text-[18px] font-medium leading-[130%] text-center md:text-left ${PPNeueMontrealFont.className} text-white mt-14`}>
+        <p className={`text-[14px] md:text-[14px] md:w-full lg:text-[16px] font-medium leading-[150%] text-center md:text-left ${PPNeueMontrealFont.className} text-white lg:mt-14`}>
           Dotslash, the annual extravaganza by the College of Engineering, Trivandrum's Computer Science & Engineering Department, showcases student innovation. It includes workshops, competitions, and project exhibitions, providing hands-on experience, exposure to technologies, and networking opportunities. Evolving over the years, Dotslash builds a legacy of innovation, fostering a culture of continuous learning and skill development in the computer science community.
         </p>
       </div>
