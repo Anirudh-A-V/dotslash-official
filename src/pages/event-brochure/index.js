40d0fe8ffb2ae47bc01807ef1f3c094058e076f7
Anirudh A V <anirudh.av02@gmail.com>
2023-12-07 19:09:05 +0530

    Update event brochure link

--- src/pages/event-brochure/index.js
+++ src/pages/event-brochure/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-const EvenBrochureLink = 'https://drive.google.com/file/d/1vqHnh7LIyvCGPGi2hkTnXlrd_dP_dYSS/view?usp=sharing'
+const EvenBrochureLink = 'https://drive.google.com/file/d/1ru8mQjz0O_E3a4Et6mXPmSZ4fdL3A0yc/view?usp=sharing'
 
 const EventBrochure = () => {
     useEffect(() => {
